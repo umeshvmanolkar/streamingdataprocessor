9c04f057850db19e5e4bade5d849e31d1e7448b3
Umesh Manolkar <umeshvmanolkar@gmail.com>
2024-06-01 13:45:50 +0530

    project created

---
+++ src/main/java/com/streaming/data/processor/StreamingdataprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.streaming.data.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingdataprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingdataprocessorApplication.class, args);
+	}
+
+}
